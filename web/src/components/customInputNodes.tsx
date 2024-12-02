a11fcaea7d8ba1b7ad973eb65b729a38fdb9cd18
yudi-xiao <5916167+yudi-xiao@users.noreply.github.com>
2024-12-02 11:11:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ web/src/components/customInputNodes.tsx
@@ -0,0 +1,10 @@
+export const customInputNodes: Record<string, string> = {
+  ComfyUIDeployExternalText: "string",
+  ComfyUIDeployExternalImage: "string - (public image url)",
+  ComfyUIDeployExternalImageAlpha: "string - (public image url)",
+  ComfyUIDeployExternalNumber: "float",
+  ComfyUIDeployExternalNumberInt: "integer",
+  ComfyUIDeployExternalLora: "string - (public lora download url)",
+  ComfyUIDeployExternalCheckpoint: "string - (public checkpoints download url)",
+  ComfyUIDeployExternalFaceModel: "string - (public face model download url)",
+};
